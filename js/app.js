462213c8541d2db2ad41e69ea59f3089eb537150
kmb64 <km_balemi@hotmail.com>
2014-04-19 18:18:55 +1000

    Adding logic to calculate number of mines above, to the right or left and below a cell.

--- js/app.js
+++ js/app.js
@@ -18,7 +18,24 @@ var kbMinesweeper = (function(angular) {
         DEATH_MINE_CLASS = 'death-mine',
         X_MINE_CLASS = 'x-mine';
 
+    var TYPES = [CLEAR, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT];
+
     app.service('gameBoardService', function(){
+
+        var createCells = function(){
+
+            var cells = [];
+
+            for(var i = 0; i < 81; i+=1) {
+                cells.push(
+                    {
+                        type : '',
+                        state : ''
+                    }
+                );
+            }
+            return cells;
+        };
 
         var addMines = function(cellArray, numOfMines){
             for(var i = 0; i < numOfMines; i+=1) {
@@ -30,103 +47,57 @@ var kbMinesweeper = (function(angular) {
             }
         };
 
-        var cells = [
-            { type : 'one',
-                state : 'untouched'},
-            { type : 'two'},
-            { type : 'three'},
-            { type : 'four'},
-            { type : 'five'},
-            { type : 'six'},
-            { type : 'seven'},
-            { type : 'eight'},
-            { type : ''},
+        var theresAMine = function(cell) {
+          return cell.type === MINE;
+        };
 
-            { type : ''},
-            { type : ''},
-            { type : 'clear'},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
+        var minesBesideMe = function(index, cellsArray) {
+            var count = 0;
+            //Check above
+            if(index > 8) {
+                if(theresAMine(cellsArray[index - 9])) {
+                    count +=1;
+                }
+            }
+            //Check right
+            if(index % 9 !== 8) {
+                if(theresAMine(cellsArray[index + 1])) {
+                    count += 1;
+                }
+            }
+            //Check below
+            if(index < 71) {
+                if(theresAMine(cellsArray[index + 9])) {
+                    count += 1;
+                }
+            }
+            //Check left
+            if(index % 9 !== 0) {
+                if(theresAMine(cellsArray[index - 1])) {
+                    count += 1;
+                }
+            }
+            return count;
+        };
 
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
 
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''},
-            { type : ''}
-        ];
+        var addNumbers = function(cells) {
+            var i = 0;
+            angular.forEach(cells, function(cell){
+                //If this cell isn't a mine, check how many are near it.
+                if(cell.type !== MINE) {
+                    cell.type = TYPES[minesBesideMe(i, cells)];
+                }
+                i+=1;
+            });
+        };
 
         return {
             generate : function(){
 
+                var cells = createCells();
                 addMines(cells, 10);
+                addNumbers(cells);
 
                 return {
                     cells : cells
