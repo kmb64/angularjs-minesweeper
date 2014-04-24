0ad46bcbf255524b7490ecf189d1a19b34b6b902
Karl Balemi <karl.balemi@auspost.com.au>
2014-04-24 14:16:55 +1000

    Implemented auto cell clearing with simultaneous left and right mouse click.

--- js/directives.js
+++ js/directives.js
@@ -2,6 +2,9 @@ app.directive('cell', ['gameBoardService', function(gameBoardService){
 
     function link(scope, element, attrs) {
 
+      var rightMouseDown = false,
+        leftMouseDown = false;
+
         element.bind('click', function(){
             if(scope.cell.type === CLEAR) {
                 gameBoardService.clearSurroundingCells(scope.cell.index);
@@ -35,6 +38,32 @@ app.directive('cell', ['gameBoardService', function(gameBoardService){
             }
             scope.$apply();
         });
+
+      element.bind('mousedown', function(event){
+        switch (event.which) {
+          //Left mouse button
+          case 1:
+            leftMouseDown = true;
+            break;
+          //Right mouse button
+          case 3:
+            rightMouseDown = true;
+            break;
+          default:
+        }
+
+      });
+
+      element.bind('mouseup', function(){
+        if(leftMouseDown && rightMouseDown) {
+          leftMouseDown = false;
+          rightMouseDown = false;
+          if(scope.cell.state !== UNTOUCHED && scope.cell.state !== FLAGGED && scope.cell.state !== UNSURE) {
+            gameBoardService.clearIfFlagged(scope.cell.index);
+          }
+          scope.$apply();
+        }
+      });
     }
 
     return {
