5de32db0d2ecef10359e290339d65ded6a0c1724
kmb64 <km_balemi@hotmail.com>
2014-04-28 21:53:38 +1000

    Arranging icons on scoreboard.

--- js/app.js
+++ js/app.js
@@ -14,9 +14,23 @@ var ONE = 'one',
     UNSURE = 'unsure',
     MINE = 'mine',
     DEATH_MINE_CLASS = 'death',
-    MINE_COUNT = 99,
+
+//    MINE_COUNT = 10,
+//    ROWS = 9,
+//    COLS = 9,
+
+    MINE_COUNT = 40,
     ROWS = 16,
-    COLS = 30,
+    COLS = 16,
+
+//    MINE_COUNT = 99,
+//    ROWS = 16,
+//    COLS = 30,
+
     CELL_COUNT = ROWS * COLS;
+
+    //beginner : 9 x 9 (10)
+    //intermediate : 16 x 16 (40)
+    //expert : 16 x 30 (99)
 
 var TYPES = [CLEAR, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT];
