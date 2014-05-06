eace243c1b889c0f4a26ee5c9274817dfd2bb09b
Karl Balemi <karl.balemi@auspost.com.au>
2014-05-06 13:09:19 +1000

    Angular cookie module.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('kbMinesweeper', []);
+var app = angular.module('kbMinesweeper', ['ngCookies']);
 
 var ONE = 'one',
   TWO = 'two',
@@ -19,20 +19,5 @@ var ONE = 'one',
   rows,
   cols,
   cellCount;
-
-//cellCount = rows * cols;
-
-//    mineCount = 40,
-//    rows = 16,
-//    cols = 16,
-
-//    mineCount = 99,
-//    rows = 16,
-//    cols = 30,
-
-
-//    beginner : 9 x 9 (10)
-//    intermediate : 16 x 16 (40)
-//    expert : 16 x 30 (99)
 
 var TYPES = [CLEAR, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT];
